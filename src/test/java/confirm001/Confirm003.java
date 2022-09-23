61500527268bb620c340c641215765266a8bd668
akshay <akshaypkamble393@gmail.com>
2022-09-23 18:24:41 +0530

    003

---
+++ src/test/java/confirm001/Confirm003.java
@@ -0,0 +1,16 @@
+package confirm001;
+
+import org.testng.annotations.Test;
+
+public class Confirm003 {
+	
+	@Test
+	public void contact()
+	{
+		System.out.println("This is my first commit - MavenProjects");
+		
+		System.out.println("ok");
+	}
+
+
+}
